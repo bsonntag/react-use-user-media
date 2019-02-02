2edff0e53ac31280b0ccedfce4f9bad95cd9b457
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-02-02 20:28:26 +0000

    Update react-use-promise

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import stopMediaStream from 'stop-media-stream';
 import usePromise from 'react-use-promise';
 
 function useUserMedia(constraints) {
-  const [stream, error] = usePromise(() => {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  });
+  const [stream, error, state] = usePromise(useMemo(
+    () => navigator.mediaDevices.getUserMedia(constraints),
+    [constraints]
+  ));
 
   useEffect(() => () => stopMediaStream(stream), [stream]);
 
-  return { error, stream };
+  return { error, state, stream };
 }
 
 export default useUserMedia;
