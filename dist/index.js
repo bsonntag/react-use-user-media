605b4dfd3feb6c0dc76a99b62c2464d82f7cc8e9
rickstaa <rick.staa@outlook.com>
2021-07-21 08:21:21 +0200

    0.1.0

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports["default"] = exports.useUserMedia = void 0;
 
 var _react = require("react");
 
@@ -11,47 +11,88 @@ var _stopMediaStream = _interopRequireDefault(require("stop-media-stream"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-function useUserMedia(constraints) {
-  var _useState = (0, _react.useState)(),
-      _useState2 = _slicedToArray(_useState, 2),
-      stream = _useState2[0],
-      setStream = _useState2[1];
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
-  var _useState3 = (0, _react.useState)(),
-      _useState4 = _slicedToArray(_useState3, 2),
-      error = _useState4[0],
-      setError = _useState4[1];
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-  var _useState5 = (0, _react.useState)('pending'),
-      _useState6 = _slicedToArray(_useState5, 2),
-      state = _useState6[0],
-      setState = _useState6[1];
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-  (0, _react.useDebugValue)({
-    error: error,
-    state: state,
-    stream: stream
-  });
+/**
+ * Reducer that handles all useUserMedia states and actions.
+ */
+var mediaStateReducer = function mediaStateReducer(curMediaState, action) {
+  switch (action.type) {
+    case 'GET':
+      return _objectSpread(_objectSpread({}, curMediaState), {}, {
+        state: 'pending'
+      });
+
+    case 'RESPONSE':
+      return _objectSpread(_objectSpread({}, curMediaState), {}, {
+        state: 'resolved',
+        stream: action.stream
+      });
+
+    case 'ERROR':
+      return _objectSpread(_objectSpread({}, curMediaState), {}, {
+        error: action.error,
+        state: 'rejected'
+      });
+
+    default:
+      throw new Error("Action type ".concat(action.type, " not supported by the mediaStateReducer"));
+  }
+};
+/**
+ * React hook for accessing user media.
+ * This hook ensures that getUserMedia is only called one time.
+ *
+ * @remarks Please make sure you wrap your constraint object inside a useEffect or
+ * useMemo hook to prevent infinite render loops.
+ */
+
+
+var useUserMedia = function useUserMedia(constraints) {
+  var _useReducer = (0, _react.useReducer)(mediaStateReducer, {
+    error: null,
+    state: 'pending',
+    stream: null
+  }),
+      _useReducer2 = _slicedToArray(_useReducer, 2),
+      userMediaState = _useReducer2[0],
+      dispatchUserMedia = _useReducer2[1];
+
+  (0, _react.useDebugValue)(userMediaState);
   (0, _react.useEffect)(function () {
     var canceled = false;
-    setState('pending');
+    dispatchUserMedia({
+      type: 'GET'
+    });
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
       if (!canceled) {
-        setState('resolved');
-        setStream(stream);
+        dispatchUserMedia({
+          stream: stream,
+          type: 'RESPONSE'
+        });
       }
     }, function (error) {
       if (!canceled) {
-        setState('rejected');
-        setError(error);
+        dispatchUserMedia({
+          error: error,
+          type: 'ERROR'
+        });
       }
     });
     return function () {
@@ -60,15 +101,12 @@ function useUserMedia(constraints) {
   }, [constraints]);
   (0, _react.useEffect)(function () {
     return function () {
-      return (0, _stopMediaStream["default"])(stream);
+      return (0, _stopMediaStream["default"])(userMediaState.stream);
     };
-  }, [stream]);
-  return {
-    error: error,
-    state: state,
-    stream: stream
-  };
-}
+  }, [userMediaState.stream]);
+  return userMediaState;
+};
 
+exports.useUserMedia = useUserMedia;
 var _default = useUserMedia;
 exports["default"] = _default;
