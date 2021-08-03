ef09e2c26caabc6ba823dc0c7bc04a53fca49ba1
rickstaa <rick.staa@outlook.com>
2021-08-03 15:09:16 +0200

    Improves docstrings

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,6 +5,8 @@ export interface UserMediaState {
 }
 /**
  * React hook for accessing user media.
+ * @param constraints - The media stream constraints.
+ * @returns The user media state object.
  *
  * @remarks Please make sure you wrap your constraint object inside a useEffect or
  * useMemo hook to prevent infinite render loops.
--- dist/index.js
+++ dist/index.js
@@ -160,6 +160,8 @@ var mediaStateReducer = function mediaStateReducer(curMediaState, action) {
 };
 /**
  * React hook for accessing user media.
+ * @param constraints - The media stream constraints.
+ * @returns The user media state object.
  *
  * @remarks Please make sure you wrap your constraint object inside a useEffect or
  * useMemo hook to prevent infinite render loops.
